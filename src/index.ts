d7aafea52a00e55596e237e0dfd4f50679333382
Argtum <silwar40k@gmail.com>
2021-06-14 22:05:38 +0300

    create company class, connect with google maps by @types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello? there");
+/*import { User } from './User';
+import { Company } from './Company';*/
+
+new google.maps.Map(document.querySelector("#map"), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
